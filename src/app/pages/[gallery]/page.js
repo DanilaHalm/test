a65884000d8c2957ecc046990ec61bc96353d491
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-09-21 09:56:06 +0300

    Update page.js

--- src/app/pages/[gallery]/page.js
+++ src/app/pages/[gallery]/page.js
@@ -1,7 +1,4 @@
-
-
-const Gallery = async ({params}) => {
-  
+export async function generateStaticParams() {
   const res = await fetch("https://parseapi.back4app.com/classes/galleryTest", {
     method: "GET",
     headers: {
@@ -11,12 +8,28 @@ const Gallery = async ({params}) => {
     },
     next: { revalidate : 10 }
   });
-  const { results } = await res.json();
+  const { results } = await res.json()
+ 
+  return results
+}
+
+const Gallery = async ({params}) => {
+  
+  //const res = await fetch("https://parseapi.back4app.com/classes/galleryTest", {
+   // method: "GET",
+  //  headers: {
+     // "X-Parse-Application-Id": "M7ajsoUJdW6shmY2c3zZtGlbvUvh8ejuq62mnkT2",
+    //  "X-Parse-REST-API-Key": "vEa4z74M4ZXql3x3QtujV9sgLnGBcWNULRPh1itf",
+   //   "content-type": "application/json",
+   // },
+  //  next: { revalidate : 10 }
+  //});
+  //const { results } = await res.json();
   
   
   
   return (
-    <main> {results?.length} 
+    <main> {params?.length} 
   <div></div>
   </main>
   )
