4271aef8a410b53e12836a618b2e28767e434fac
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-09-21 19:31:09 +0300

    Update page.js

--- src/app/pages/[gallery]/page.js
+++ src/app/pages/[gallery]/page.js
@@ -1,18 +1,4 @@
-export async function generateStaticParams() {
-  const res = await fetch("https://parseapi.back4app.com/classes/galleryTest", {
-    method: "GET",
-    headers: {
-      "X-Parse-Application-Id": "M7ajsoUJdW6shmY2c3zZtGlbvUvh8ejuq62mnkT2",
-     "X-Parse-REST-API-Key": "vEa4z74M4ZXql3x3QtujV9sgLnGBcWNULRPh1itf",
-     "content-type": "application/json",
-   },
-    next: { revalidate : 10 }
- });
-  const { results } = await res.json()
-  const galleryData = JSON.stringify(results)
-  const a = [{"gallery": galleryData }]
-  return a
-}
+
 
 const Gallery = ({params}) => {
   
@@ -30,7 +16,7 @@ const Gallery = ({params}) => {
   
   
   return (
-    <main> {params?.length} 
+    <main> 
   <div></div>
   </main>
   )
