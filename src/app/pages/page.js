e8986a8d0266bff2ef3ab1fb2ab94fb332bcb79b
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-09-21 10:15:00 +0300

    Create page.js

---
+++ src/app/pages/page.js
@@ -0,0 +1,20 @@
+export async function generateStaticParams() {
+  const res = await fetch("https://parseapi.back4app.com/classes/galleryTest", {
+    method: "GET",
+    headers: {
+      "X-Parse-Application-Id": "M7ajsoUJdW6shmY2c3zZtGlbvUvh8ejuq62mnkT2",
+      "X-Parse-REST-API-Key": "vEa4z74M4ZXql3x3QtujV9sgLnGBcWNULRPh1itf",
+      "content-type": "application/json",
+    },
+    next: { revalidate : 10 }
+  });
+  const { results } = await res.json()
+  const a = [{"gallery" : results}]
+  return a
+}
+
+const ComPage = () => {
+  return<div></div>
+}
+
+export default ComPage
