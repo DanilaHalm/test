56f05ea218b66e1957b8c80daff188494b192b27
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-09-21 08:58:37 +0300

    Create getGalleryAlbums.js

---
+++ src/app/api/getGalleryAlbums.js
@@ -0,0 +1,14 @@
+const getGalleryAlbums = async () => {
+  const res = await fetch("https://parseapi.back4app.com/classes/galleryTest", {
+    method: "GET",
+    headers: {
+      "X-Parse-Application-Id": "M7ajsoUJdW6shmY2c3zZtGlbvUvh8ejuq62mnkT2",
+      "X-Parse-REST-API-Key": "vEa4z74M4ZXql3x3QtujV9sgLnGBcWNULRPh1itf",
+      "content-type": "application/json",
+    },
+  });
+  const { results } = await res.json();
+  return results;
+};
+
+export default getGalleryAlbums;
