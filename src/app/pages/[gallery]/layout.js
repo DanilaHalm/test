b8ffef637524654699a498a772f7f9f5483bbb7f
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-09-21 19:35:25 +0300

    Create layout.js

---
+++ src/app/pages/[gallery]/layout.js
@@ -0,0 +1,23 @@
+export async function generateStaticParams() {
+  const a = [{"gallery":"gallery"}]
+  return a
+}
+
+const Layout = ({children})=>{
+  const res = await fetch("https://parseapi.back4app.com/classes/galleryTest", {
+    method: "GET",
+    headers: {
+      "X-Parse-Application-Id": "M7ajsoUJdW6shmY2c3zZtGlbvUvh8ejuq62mnkT2",
+     "X-Parse-REST-API-Key": "vEa4z74M4ZXql3x3QtujV9sgLnGBcWNULRPh1itf",
+     "content-type": "application/json",
+   },
+    next: { revalidate : 10 }
+ });
+  const { results } = await res.json()
+
+  return (
+    <main>
+    {children data={results}}
+    </main>
+  )
+}
