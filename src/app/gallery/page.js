2a579ef5c3824a7c782e8c671274ecd976dc912e
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-09-21 09:24:27 +0300

    Update page.js

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,9 +1,18 @@
 import getGalleryAlbums from "@/app/api/getGalleryAlbums"
 
-export const revalidate = 10
-
 const Gallery = async () => {
-  const items = await getGalleryAlbums()
+  const items = await fetch("https://parseapi.back4app.com/classes/galleryTest", {
+    method: "GET",
+    headers: {
+      "X-Parse-Application-Id": "M7ajsoUJdW6shmY2c3zZtGlbvUvh8ejuq62mnkT2",
+      "X-Parse-REST-API-Key": "vEa4z74M4ZXql3x3QtujV9sgLnGBcWNULRPh1itf",
+      "content-type": "application/json",
+    },
+    next: { revalidate : 10 }
+  });
+  const { results } = await res.json();
+  return results;
+};
   
   
   return (
