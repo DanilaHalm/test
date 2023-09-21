5dd99b62c2bbfd62cd1d0b67971bcc756512e817
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-09-21 12:33:16 +0300

    Update page.js

--- src/app/pages/page.js
+++ src/app/pages/page.js
@@ -1,17 +1,17 @@
-export async function generateStaticParams() {
-  const res = await fetch("https://parseapi.back4app.com/classes/galleryTest", {
-    method: "GET",
-    headers: {
-      "X-Parse-Application-Id": "M7ajsoUJdW6shmY2c3zZtGlbvUvh8ejuq62mnkT2",
-      "X-Parse-REST-API-Key": "vEa4z74M4ZXql3x3QtujV9sgLnGBcWNULRPh1itf",
-      "content-type": "application/json",
-    },
-    next: { revalidate : 10 }
-  });
-  const { results } = await res.json()
-  const a = [{1:"1"},{2:"2"},{3:"3"}]
-  return a
-}
+//export async function generateStaticParams() {
+//  const res = await fetch("https://parseapi.back4app.com/classes/galleryTest", {
+//    method: "GET",
+//    headers: {
+//      "X-Parse-Application-Id": "M7ajsoUJdW6shmY2c3zZtGlbvUvh8ejuq62mnkT2",
+//      "X-Parse-REST-API-Key": "vEa4z74M4ZXql3x3QtujV9sgLnGBcWNULRPh1itf",
+//      "content-type": "application/json",
+//    },
+//    next: { revalidate : 10 }
+//  }) 
+//  const { results } = await res.json()
+//  const a = [{1:"1"},{2:"2"},{3:"3"}]
+//  return a
+//}
 
 const ComPage = () => {
   return<div></div>
