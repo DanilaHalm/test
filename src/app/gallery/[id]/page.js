f04281c1be626c60cb665fe31279f33af8abb3b9
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-09-25 12:02:33 +0300

    Create page.js

---
+++ src/app/gallery/[id]/page.js
@@ -0,0 +1,44 @@
+import getGalleryAlbums from "@/app/api/getGalleryAlbums"
+
+export const revalidate= 10
+
+export const generateStaticParams= async () {
+  const res = await fetch("https://parseapi.back4app.com/classes/galleryTest", {
+    method: "GET",
+    headers: {
+      "X-Parse-Application-Id": "M7ajsoUJdW6shmY2c3zZtGlbvUvh8ejuq62mnkT2",
+      "X-Parse-REST-API-Key": "vEa4z74M4ZXql3x3QtujV9sgLnGBcWNULRPh1itf",
+      "content-type": "application/json",
+    },
+    
+  });
+  const { results } = await res.json();
+  return results.map(item=>{
+    id : item.objectId
+  })
+}
+
+const GalleryItem = async ({params}) => {
+  const {id} = params
+  const res = await fetch(`https://parseapi.back4app.com/classes/galleryTest/${id}`, {
+    method: "GET",
+    headers: {
+      "X-Parse-Application-Id": "M7ajsoUJdW6shmY2c3zZtGlbvUvh8ejuq62mnkT2",
+      "X-Parse-REST-API-Key": "vEa4z74M4ZXql3x3QtujV9sgLnGBcWNULRPh1itf",
+      "content-type": "application/json",
+    },
+    
+  });
+  const  result = await res.json();
+  
+ // const items = JSON.stringify(params)
+  
+  
+  return (
+    <main> 
+  <div>{result?.title}</div>
+  </main>
+  )
+}
+
+export default GalleryItem
