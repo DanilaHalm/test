bbceb9ec5fa047211a368c55ba58c1bfcf766833
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-09-25 12:09:46 +0300

    Update page.js

--- src/app/gallery/[id]/page.js
+++ src/app/gallery/[id]/page.js
@@ -3,16 +3,7 @@ import getGalleryAlbums from "@/app/api/getGalleryAlbums"
 export const revalidate= 10
 
 export const generateStaticParams= async () {
-  const res = await fetch("https://parseapi.back4app.com/classes/galleryTest", {
-    method: "GET",
-    headers: {
-      "X-Parse-Application-Id": "M7ajsoUJdW6shmY2c3zZtGlbvUvh8ejuq62mnkT2",
-      "X-Parse-REST-API-Key": "vEa4z74M4ZXql3x3QtujV9sgLnGBcWNULRPh1itf",
-      "content-type": "application/json",
-    },
-    
-  });
-  const { results } = await res.json();
+  const results = await getGalleryAlbums();
   return results.map(item=>{
     id : item.objectId
   })
