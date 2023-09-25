8a5649dd9ace2ccae10aea64549fa3c888ad88fc
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-09-25 21:33:09 +0300

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,12 @@
-"use client"
+//"use client"
 
 import Link from "next/link";
-import { useRouter } from 'next/navigation'
+//import { useRouter } from 'next/navigation'
 
 const Nav = () => {
-const router = useRouter()
- 
- return (<button onClick={() => router.push('/gallery')}>gallery</button>)
+//const router = useRouter()
+ return (<Link href={"/gallery"} >gallery</Link>)
+ //return (<button onClick={() => router.push('/gallery')}>gallery</button>)
 }
 
 export default Nav
