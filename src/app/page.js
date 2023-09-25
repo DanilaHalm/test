fea9e197e758643748d08675357488d5b20fe0af
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-09-25 15:07:41 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,9 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24">hello</main>;
+  return <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <Link href={"/gallery"}>
+    Gallery 
+    </Link>
+    </main>;
 }
