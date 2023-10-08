7bd7e7355724e86c944cd252fdd96ee19a2e5078
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-10-08 18:41:30 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Nav from "@/components/Nav"
 import myAction from "@/components/myaction"
 
-export default function Home() {
+const Home = () => {
   return (<main className="flex min-h-screen flex-col items-center justify-between p-24">
     <Nav />
     <form>
     <input type="search"/>
-    <form/>
+    </form>
     </main>)
 }
+export default Home
