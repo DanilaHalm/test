9d66832c92bee12fc139fb63276e95b39c4d2d0a
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-10-08 16:24:19 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,5 +3,6 @@ import Nav from "@/components/Nav"
 export default function Home() {
   return <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <Nav />
+    <input type="search"/>
     </main>;
 }
