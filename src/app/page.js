09c4c8c45abe3079d8cb5f722a81e3635a03131a
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-10-08 18:34:48 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,10 @@ import Nav from "@/components/Nav"
 import myAction from "@/components/myaction"
 
 export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24">
+  return (<main className="flex min-h-screen flex-col items-center justify-between p-24">
     <Nav />
     <form>
-    <input type={"search"}/>
+    <input type="search"/>
     <form/>
-    </main>;
+    </main>)
 }
