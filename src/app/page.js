ba8e912ac7485c9b8dee931edb1dd1fc244bae02
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-10-08 17:40:56 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <Nav />
     <form>
-    <input type="search"/>
+    <input type={"search"}/>
     <form/>
     </main>;
 }
