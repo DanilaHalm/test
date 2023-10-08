594a4603b6a1c13eaa2944bd785af307dc4c5407
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-10-08 16:39:20 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Nav from "@/components/Nav"
+import myAction from "@/components/myaction"
 
 export default function Home() {
   return <main className="flex min-h-screen flex-col items-center justify-between p-24">
