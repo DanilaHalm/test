2f78f031992f2e736a1fb79057f7f88ebb0a1d19
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-10-10 17:04:17 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (<main className="flex min-h-screen flex-col items-center justify-between p-24">
     <Nav />
     <form action="http://google.com/search" method="get" target="_blank">
-    <input type="search"/>
+    <input name="q" type="search"/>
     </form>
     </main>)
 }
