59349aa7c4dfa44a34a9c08f730721b6883de1f9
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-10-11 02:18:24 +0300

    Create Weather.jsx

---
+++ src/components/Weather.jsx
@@ -0,0 +1,24 @@
+
+const Weather = async () => {
+  const baseUrl = "http://api.weatherapi.com/v1/current.json?"
+  const apiKey = "58f433ea3730490c911225705231010"
+  const city = "Novolukoml"
+  const res = await fetch(`${baseUrl}key=${apiKey}&q=${city}&aqi=no`)
+  const data = await res.json()
+
+  const location = data?.location.name
+  const temp = data?.current.temp_c
+  const condition= data?.current.condition.text
+  const wind = data?.current.wind_kph
+  
+  
+  return {
+    <div>
+    <h1>{location}</h1>
+    <p>temperature: {temp} °C</p>
+    <p>condition: {condition} </p>
+    <p>wind: {wind} km/h </p>
+    </div>
+  }
+}
+export default Weather 
