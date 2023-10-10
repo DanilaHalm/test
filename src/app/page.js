b013e381a2f5996b914e81e9ec56348852f8cc13
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-10-11 01:46:11 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Nav from "@/components/Nav"
 const Home = () => {
   return (<main className="flex min-h-screen flex-col items-center justify-between p-24">
     <Nav />
-    <form action="http://google.com/search" method="get" target="_blank">
+    <form action="https://google.com/search" method="get" target="_blank">
     <input name="q" type="search"/>
     </form>
     </main>)
