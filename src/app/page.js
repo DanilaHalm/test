f7f4db01908193fb87f3024ac8437c3b17d8de6f
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-10-11 22:05:30 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import Nav from "@/components/Nav"
 import Weather from "@/components/Weather"
 
+export const dynamic = "force-dynamic"
+
 const Home = () => {
   return (<main className="flex min-h-screen flex-col items-center justify-between p-24">
     <Nav />
